dff3c12ea4bad060d6fa220535d95dbb294f66a3
Vlasova Mariia <vlasovam014@gmail.com>
2025-04-02 16:39:53 +0300

    clean repository

--- visual_math/apps/presentations/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from apps.presentations.consumers import PresentationConsumer
-
-websocket_urlpatterns = [
-    re_path(r"ws/presentation/(?P<presentation_id>\w+)/$", PresentationConsumer.as_asgi()),
-]
